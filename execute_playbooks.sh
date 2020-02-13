4b4ce7e7d9846d3ae78dfe3e5db993ae15ebc9b5
Dario Martins Silva <dmsilva@protonmail.com>
2020-02-13 13:22:30 -0500

    only install metasploit when not kali

--- execute_playbooks.sh
+++ execute_playbooks.sh
@@ -8,25 +8,28 @@ title() {
 
 title "Install zsh"
 sudo ansible-galaxy install viasite-ansible.zsh --force
-sudo ansible-playbook -i "localhost," -c local ./zsh.yml
+ansible-galaxy install viasite-ansible.zsh --force
+ansible-playbook -i "localhost," -c local ./zsh.yml
 sudo ansible-playbook -i "localhost," -c local ./zsh.yml --extra-vars="zsh_user=$(whoami)"
 
-title "Install basic utilities"
-sudo ansible-playbook -i "localhost," -c local ./basics.yml
+title "Install & configure basic utilities"
+sudo ansible-playbook -i "localhost," -c local ./update.yml
+ansible-playbook -i "localhost," -c local ./basics.yml
+sudo ansible-playbook -i "localhost," -c local ./sudo_basics.yml
 
 title "Install vscode"
-sudo ansible-galaxy install gantsign.visual-studio-code --force
-sudo ansible-playbook -i "localhost," -c local ./vscode.yml
+ansible-galaxy install gantsign.visual-studio-code --force
+ansible-playbook -i "localhost," -c local ./vscode.yml
 
-title "Install metasploit"
-sudo ansible-playbook -i "localhost," -c local ./metasploit.yml
+os_string=`cat /etc/issue`
 
-title "Install vim"
-sudo ansible-galaxy install franklinkim.vim --force
-sudo ansible-playbook -i "localhost," -c local ./vim.yml
+if [[ $os_string != *"Kali"* ]]; then
+  title "Install metasploit"
+  ansible-playbook -i "localhost," -c local ./metasploit.yml
+fi
 
 title "Install pwndbg"
-sudo ansible-playbook -i "localhost," -c local ./pwndbg.yml
+ansible-playbook -i "localhost," -c local ./pwndbg.yml
 
 
 
